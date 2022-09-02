4aca255821c3eb336ee0e9f7854e98edb3260bdd
Manasa6268 <giddalapatimanasa@gmail.com>
2022-09-02 15:20:06 +0530

    Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice-app';
+  color = 'red'
+  status = 'no event happened'
+  text = ''
+  textcolor = 'white'
+  allow: boolean = false
+  ontextchange: boolean = false
+  constructor() {
+    setTimeout(() => {
+      this.allow = true
+      this.color = 'blue'
+      if (this.allow) {
+        window.alert('button will enable now');
+      }
+      else {
+        window.alert('button will disable now');
+      }
+    }, 3000)
+  }
+  onBtnClick() {
+    window.alert('Button was clicked and text  ' + this.text + ' was entered');
+    this.ontextchange = true
+  }
+  onUpdate(event: any) {
+    this.text = event.target.value;
+
+  }
 }
